4b1473a8776da3c6f6ab23108a4fadbfb5ca984f
DemonSword09 <72071947+DemonSword09@users.noreply.github.com>
2022-04-20 00:51:38 +0530

    Update constants.ts
    Update

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,8 +1,4 @@
 export const SECRECT = "sdjbasfkhbjasgdfaskjdf";
 export const __prod__ = process.env.NODE_ENV !== "production";
-export const MONGODB_URI =
-  "mongodb+srv://harsh-admin:wqsIjF9GxD8YFF4o@cluster0.n7mse.mongodb.net/Carparking?retryWrites=true&w=majority";
-export const FORGOT_PASSWORD_PREFIX = "forgotpassword:";
 
-export const RAZOR_PAY_KEY_ID = "rzp_test_MI4VoHVqRnJPx9";
-export const RAZOR_PAY_KEY_SECRET = "Tmt7b85a7GGqlgXs0GFLSIOO";
+export const FORGOT_PASSWORD_PREFIX = "forgotpassword:";
